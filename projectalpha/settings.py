e31e68c5ec9900ca15c743c7c03e72c760d4a67f
tabspacecoder <jc.mugunth@gmail.com>
2025-05-26 16:42:42 +0530

    modified settings to add cors

--- projectalpha/settings.py
+++ projectalpha/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restapp',
     'storages',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'projectalpha.urls'
