21a2a04fc5435ef7e689b65807706fff97e65866
tabspacecoder <jc.mugunth@gmail.com>
2025-05-26 16:53:01 +0530

    modified requirement and model

--- restapp/models.py
+++ restapp/models.py
@@ -10,3 +10,13 @@ class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.file and self._state.adding:
+            self.process_file(self.file)
+        super().save(*args, **kwargs)
+
+    def process_file(self, uploaded_file):
+        uploaded_file.seek(0)
+        file_data = uploaded_file.read()
+        print(f"File size: {len(file_data)} bytes")
