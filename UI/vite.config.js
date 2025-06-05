fadf8643d684de468b94ba12b727a538e8ec2d2e
Karthick <karthicksudeeth@gmail.com>
2025-06-05 13:25:44 +0530

    Changed the port to 3000 for front end

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -8,6 +8,9 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    port: 3000
+  },
   build: {
     rollupOptions: {
       input: {
